264c41917c08fd99f205d1920454c567d456c5e2
ahmadnurjihad2023-lgtm <ahmadnurjihad.2023@student.uny.ac.id>
2026-06-05 07:12:25 +0700

    cp 3

--- UAS_23051430004_Ahmad Nurjihad/WarehouseLocator_/screens/DetailScreen.js
+++ UAS_23051430004_Ahmad Nurjihad/WarehouseLocator_/screens/DetailScreen.js
@@ -1,8 +1,22 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default function DetailScreen({ route }) {
+export default function DetailScreen({
+  route,
+  navigation
+}) {
+
   const { itemData } = route.params;
+  const [stok, setStok] = useState(itemData.stok);
+  const tambahStok = () => {
+    setStok(stok + 1);
+  };
+
+  const kurangStok = () => {
+    if (stok > 0) {
+      setStok(stok - 1);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -20,8 +34,13 @@ export default function DetailScreen({ route }) {
         </View>
 
         <View style={styles.row}>
-          <Text style={styles.label}>Stok</Text>
-          <Text style={styles.value}>{itemData.stok}</Text>
+          <Text style={styles.stok}>Stok Saat Ini : {stok} </Text>
+          
+          {stok < 5 && (
+            <Text style={styles.warning}>
+              📉 Stok Kritis
+            </Text>
+          )}
         </View>
 
         <View style={styles.row}>
@@ -29,6 +48,38 @@ export default function DetailScreen({ route }) {
           <Text style={styles.value}>{itemData.lokasiRak}</Text>
         </View>
       </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+        style={styles.button}
+        onPress={kurangStok}
+      >
+          <Text style={styles.buttonText}>
+            -
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        style={styles.button}
+        onPress={tambahStok}>
+
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={styles.backText}>
+            Kembali
+          </Text>
+
+        </TouchableOpacity>
+      </View>
+
+
     </View>
   );
 }
@@ -83,6 +134,52 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     color: "#111827",
   },
+
+  stok:{
+    fontSize:18,
+    fontWeight: 'bold',
+    marginTop:10
+  },
+
+  warning:{
+    color:'red',
+    fontWeight:'bold',
+    marginTop:10
+  },
+
+  buttonContainer:{
+    flexDirection:'row',
+    justifyContent: 'space-around',
+    marginTop:20
+  },
+
+  button:{
+    backgroundColor: '#007AFF',
+    width: 70,
+    height:70,
+    borderRadius:35,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+
+  buttonText:{
+    color:'white',
+    fontSize : 28,
+    fontWeight: 'bold'
+  },
+
+  backButton:{
+    backgroundColor: '#555',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20
+  },
+
+  backText:{
+    color: 'white',
+    textAlign: 'center',
+    fontWeight:'bold'
+  }
 });
 
 
